0987546d0b863446e4b421774e4bc3250da68ca4
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-07-17 11:18:20 +0100

    update dependencies (dotenv)

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 5000;
 
 // for development only
 const dotenv = require('dotenv');
 dotenv.config();
 
+const port = process.env.PORT || 5000;
+
 // production static route
-// app.use(express.static(path.join(__dirname, 'tbox-client/buid')))
+app.use(express.static(path.join(__dirname, 'tbox-client/buid')));
 
 app.get('/', (req, res) => {
-    res.send(`dash home, ${process.env.TEST_VAR || 'env file not here, nice!'}`)
-})
+    res.send(`dash home, ${process.env.TEST_VAR || 'env file not here, nice!'}`);
+});
 
 app.get('/test', (req,res) => {
     res.send('this will be a cool dashboard')
-})
+});
 
 app.listen(port, () => {
     console.log(`Live on port: ${port}`)
-})
+});
